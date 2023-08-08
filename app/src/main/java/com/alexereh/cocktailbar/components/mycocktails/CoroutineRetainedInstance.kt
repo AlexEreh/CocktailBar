e1fdb4f6765bb21e477c384f3cd701663fe0eeab
Alexander Ereshkin <xcode111@mail.ru>
2023-08-08 23:06:29 +0300

    Move CoroutineRetainedInstance to different file.

---
+++ app/src/main/java/com/alexereh/cocktailbar/components/mycocktails/CoroutineRetainedInstance.kt
@@ -0,0 +1,23 @@
+package com.alexereh.cocktailbar.components.mycocktails
+
+import com.arkivanov.essenty.instancekeeper.InstanceKeeper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+internal class CoroutineRetainedInstance(mainContext: CoroutineContext) : InstanceKeeper.Instance {
+    // The scope survives Android configuration changes
+    private val scope = CoroutineScope(mainContext + SupervisorJob())
+
+    fun launch(func: suspend () -> Unit) {
+        scope.launch {
+            func()
+        }
+    }
+
+    override fun onDestroy() {
+        scope.cancel() // Cancel the scope when the instance is destroyed
+    }
+}
